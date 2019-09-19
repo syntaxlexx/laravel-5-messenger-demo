ac611bfab4ce6913d48defe07182a9c0c10135ee
Lexx YungCarter <spencermainaa@gmail.com>
2019-09-19 14:30:19 +0300

    feat: Updated dependencies

--- resources/views/messenger/partials/html-message.blade.php
+++ resources/views/messenger/partials/html-message.blade.php
@@ -1,6 +1,6 @@
 <div class="media">
     <a class="pull-{{ $message->user_id == Auth::id() ? 'left' : 'left' }}" href="#">
-        <img src="//www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=64" alt="{{ $message->user->name }}" class="img-circle">
+        <img src="https://loremflickr.com/100/100/dog" alt="Avatar" class="img-circle">
     </a>
     <div class="media-body">
         <h5 class="media-heading">{{ $message->user->name }}</h5>
