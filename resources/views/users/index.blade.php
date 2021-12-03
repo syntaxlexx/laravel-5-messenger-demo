4b96aeeed97681dbc64cbb2b20a6c264c0111ebb
Lexx YungCarter <spencermainaa@gmail.com>
2021-12-04 00:01:11 +0300

    feat: users page listing now made scrollable

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,23 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
     <div class="row">
-        <div class="col-sm-6 col-md-4 col-xs-12">
-            <h3>Users in the system</h3>
-
-            <ol>
-                @forelse($users as $user)
-                    <li>{{ $user->name }} <br/><small class="text-muted">{{ $user->email }}</small></li>
-                @empty
-                    <p class="text-warning">No users found</p>
-                @endforelse
-            </ol>
-        </div>
         <div class="col-sm-6 col-md-4 col-xs-12">
             <h3>Add a User</h3>
 
             @include('users.partials.form')
 
         </div>
+
+        <div class="col-sm-6 col-md-8 col-xs-12">
+            <h3>Users in the system</h3>
+
+            <div class="list" style="height: 80vh; overflow-y: auto; overflow-x: hidden; max-width: 100%;">
+                <div class="row">
+                    @forelse($users as $user)
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <p>{{ $loop->iteration }}. {{ $user->name }} <br/><small class="text-muted">{{ $user->email }}</small></p>
+                        </div>
+                    @empty
+                        <p class="text-warning">No users found</p>
+                    @endforelse
+                </div>
+            </div>
+            
+        </div>
+        
     </div>
 @stop
